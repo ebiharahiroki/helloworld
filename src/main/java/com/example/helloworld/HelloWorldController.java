98e3545379e2504c2b2c88ede17954dd90642dae
ebiharahiroki <laoyuanhonggui@gmail.com>
2023-05-23 07:28:43 +0900

    helloエンドポイントを削除していたのを元に修正

--- src/main/java/com/example/helloworld/HelloWorldController.java
+++ src/main/java/com/example/helloworld/HelloWorldController.java
@@ -7,8 +7,8 @@ import java.util.List;
 
 @RestController
 public class HelloWorldController {
-    @GetMapping("/Bonjour")
-    public String Bonjour(){
+    @GetMapping("/hello")
+    public String hello(){
         List<String> names = List.of("hello", "bonjour", "guten tag", "buon giorno", "ciao");
         List<String> result = names.stream().filter(name -> name.contains("a")).toList();
         return result.toString();
